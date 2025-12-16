156b627de465e453d519cb484b829b450e51a1b0
Tom Prince <tprince14@gmail.com>
2025-12-16 13:44:08 -0800

    update snippet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const iframeHTML = `
     <iframe
       title="Trip Request Form"
-      src="http://localhost:8000/public/trips/embed/trip-request/?access_token=kpJlQjmjr9dE5279M5dPPV-yn7KX600__HPAjb_5VaY"
+      src="https://staging.api.hibambi.com/public/trips/embed/trip-request/?access_token=8J8aMfV7pOKY3npHwNQdXBMeoiYIm8X5WSIfb0PP_IQ"
       style="
         width: 100%;
         height: 100%;
