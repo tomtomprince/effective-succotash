2920b7e9366331636de65ba3cae08d7dedd734d8
Tom Prince <tprince14@gmail.com>
2026-02-06 08:36:02 -0800

    rawr

--- src/app/prod/page.tsx
+++ src/app/prod/page.tsx
@@ -1,24 +1,24 @@
 import { Header } from "@/components/Header";
 
 // For Bambi Internal Testing org
-const iframeHTML = `
-    <iframe
-      title="Trip Request Form"
-      src="https://api.hibambi.com/public/trips/embed/trip-request/?access_token=E9OePifaS9nMhwrx0_MLbtUJrHaRGTexiOVvh1QPgts"
-      style="
-        width: 100%;
-        height: 100%;
-      "
-    />`;
+// const iframeHTML = `
+//     <iframe
+//       title="Trip Request Form"
+//       src="https://api.hibambi.com/public/trips/embed/trip-request/?access_token=E9OePifaS9nMhwrx0_MLbtUJrHaRGTexiOVvh1QPgts"
+//       style="
+//         width: 100%;
+//         height: 100%;
+//       "
+//     />`;
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <div
+      {/* <div
         dangerouslySetInnerHTML={{ __html: iframeHTML }}
         style={{ width: "100vw", height: "100vh" }}
-      />
+      /> */}
     </main>
   );
 }
